33ba070dee22bce2e4239b9695cccaff18f743b8
“PriyankaGD” <priyanka.dastidar@gmail.com>
2023-04-12 18:00:40 -0700

    Added for Player Win/loss
    Signed-off-by: “PriyankaGD” <priyanka.dastidar@gmail.com>

--- core/src/com/gameclasses/controller/laserstrategy/LaserBindings.java
+++ core/src/com/gameclasses/controller/laserstrategy/LaserBindings.java
@@ -19,13 +19,13 @@ public class LaserBindings {
         laserStrategies = new LinkedList<>();
     }
 
-    public void fire (float deltaTime, Rectangle laserhitBox, List<EnemyLaser> enemyLaserList) {
+    public void fire (float deltaTime, Rectangle laserHitBox, List<EnemyLaser> enemyLaserList) {
         timestamp += deltaTime;
         if (currentLaserStrategy == null || (!releaseTime.isEmpty() && timestamp >= releaseTime.peek())) {
             releaseTime.poll();
             currentLaserStrategy = laserStrategies.poll();
         }
-        currentLaserStrategy.laserFire(deltaTime, laserhitBox, enemyLaserList);
+        currentLaserStrategy.laserFire(deltaTime, laserHitBox, enemyLaserList);
     }
 
     public void addLaser (String texture) {
